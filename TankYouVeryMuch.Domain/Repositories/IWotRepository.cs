c30146413c1e422ed3e17ec78099329adcc48e74
Jakub Hoczek <hoczek.jakub@gmail.com>
2023-06-18 23:52:53 +0200

    Feat: add wot repository service

---
+++ TankYouVeryMuch.Domain/Repositories/IWotRepository.cs
@@ -0,0 +1,11 @@
+﻿using DbProvider.Abstract;
+
+namespace TankYouVeryMuch.Domain.Repositories;
+
+public interface IWotRepository
+{
+    Task<Models.Entity.WotPlayerPersonalData> CreatePlayerPersonalData(Models.Dto.WotDto.WotAccountStatistics model);
+    Task<Models.Entity.WotPlayerPersonalData> UpdatePlayerPersonalData(Models.Dto.WotDto.WotAccountStatistics model);
+    Task<Models.Entity.WotPlayerPersonalData> GetPlayerPersonalData(int accountId);
+    Task<IDeleteResult> DeletePlayerPersonalData(int accountId);
+}
---
+++ TankYouVeryMuch.Domain/Repositories/WotRepository.cs
@@ -0,0 +1,50 @@
+﻿using DbProvider.Abstract;
+using TankYouVeryMuch.Domain.Mappers;
+using Entity = TankYouVeryMuch.Models.Entity;
+using Dto = TankYouVeryMuch.Models.Dto.WotDto;
+
+namespace TankYouVeryMuch.Domain.Repositories;
+
+public class WotRepository : IWotRepository
+{
+    private readonly IDataProvider<Entity.WotPlayerPersonalData> _wotDataProvider;
+
+    public WotRepository(IDataProvider<Entity.WotPlayerPersonalData> wotDataProvider)
+    {
+        _wotDataProvider = wotDataProvider;
+    }
+
+    public async Task<Entity.WotPlayerPersonalData> CreatePlayerPersonalData(Dto.WotAccountStatistics model)
+    {
+        var entity = model.ToEntity();
+        
+        await _wotDataProvider.Create(entity);
+
+        return entity;
+    }
+
+    public async Task<Entity.WotPlayerPersonalData> UpdatePlayerPersonalData(Dto.WotAccountStatistics model)
+    {
+        var dbEntity = (await _wotDataProvider.Find(x => x.Nickname == model.Nickname || x.AccountId == model.AccountId)).FirstOrDefault();
+
+        if (dbEntity == null)
+            throw new InvalidOperationException($"Account {model.AccountId} does not exist in database");
+
+        var updateEntity = model.ToEntity();
+        updateEntity.Id = dbEntity.Id;
+
+        await _wotDataProvider.Update(updateEntity);
+
+        return updateEntity;
+    }
+
+    public async Task<Entity.WotPlayerPersonalData> GetPlayerPersonalData(int accountId)
+    {
+        return (await _wotDataProvider.Find(x => x.AccountId == accountId)).FirstOrDefault() ?? throw new Exception("Account not found");
+    }
+
+    public async Task<IDeleteResult> DeletePlayerPersonalData(int accountId)
+    {
+       return await _wotDataProvider.Delete(x => x.AccountId == accountId);
+    }
+}
